f75270e89782312ae9383133ff56a10dd704fb6a
Eric Hoang <hoanger@oregonstate.edu>
2021-01-14 16:01:20 -0800

    first android studio work

---
+++ AndroidStudioProjects/CS492Todos/app/src/main/java/com/example/cs492todos/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.cs492todos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText todoEntryET;
+    private TextView todoListTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.todoListTV = findViewById(R.id.tv_todo_list);
+        this.todoListTV.setText("TODO: finish my TODO app");
+
+        this.todoEntryET = findViewById(R.id.et_todo_entry_box);
+
+        Button addTodoBtn = findViewById(R.id.btn_add_todo);
+        
+    }
+}
